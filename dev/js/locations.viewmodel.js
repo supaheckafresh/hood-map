8f4fdcb50c6c9e12da50d01c5640396eb7a659da
supaheckafresh <neilstrain@gmail.com>
2016-02-24 14:05:42 -0800

    Implement suppression of locations outside of current map boundary

--- dev/js/locations.viewmodel.js
+++ dev/js/locations.viewmodel.js
@@ -12,18 +12,23 @@ var LocationsViewModel = function (mapVm) {
     // TODO: Give search results only for current map boundaries.
     vm.searchActivityLocations = function (activity) {
 
+        var mapBounds = mapVm.mapCopy.getBounds();
+
         mapVm.placesService.textSearch({
-            bounds: mapVm.mapCopy.getBounds(),
+            bounds: mapBounds,
             query: activity
         }, callback);
 
         function callback(results, status) {
 
             if (results.length > 0) {
+
+                var filteredResults = suppressOutOfBoundsLocations(results);
+
                 // Transform the resulting list from Google Places to an object and push to `locationGroups`.
                 vm.locationGroups.push({
                     activity: activity,
-                    results: results
+                    results: filteredResults
                 });
             } else {
                 alert('Sorry, there were no results for that activity.');
@@ -31,11 +36,29 @@ var LocationsViewModel = function (mapVm) {
 
             // Add location markers for resulting places.
             if (status === google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0, len = results.length; i < len; i++) {
-                    console.log(results[i].name);
-                    mapVm.addMarker(results[i]);
+                for (var i = 0, len = filteredResults.length; i < len; i++) {
+
+                    mapVm.addMarker(filteredResults[i]);
                 }
             }
         }
-    };
+
+        function suppressOutOfBoundsLocations(results) {
+            var locLat, locLng;
+            var inBoundResults = [];
+            _.each(results, function (location) {
+                locLat = location.geometry.location.lat();
+                locLng = location.geometry.location.lng();
+
+                // Store each `location` only if it is contained within the map boundaries at current zoom level.
+                if (mapBounds.R.R < locLat && locLat < mapBounds.R.j &&
+                    mapBounds.j.R > locLng && locLng > mapBounds.j.j) {
+
+                    inBoundResults.push(location);
+                }
+            });
+
+            return inBoundResults;
+        }
+    }
 };
