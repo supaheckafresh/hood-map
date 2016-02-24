e3cf08b844c639a0b07f66e8396fbbb0d4cc8359
supaheckafresh <neilstrain@gmail.com>
2016-02-24 14:51:34 -0800

    Refactor ActivititesViewModel and add TODO

--- dev/js/activities.viewmodel.js
+++ dev/js/activities.viewmodel.js
@@ -12,11 +12,18 @@ var ActivitiesViewModel = function (mapVm, locationsVm) {
     vm.addActivity = function () {
 
         // TODO: Validate input and handle duplicates (also what do do after map is resized).
+        // TODO: Decide whether to display the desired activity in the UI even if there are no location results
+        // (todo cont...) in the current map.
 
         vm.activities.push(vm.newActivity());
+
+        vm.displayActivityLocations();
+    };
+
+    vm.displayActivityLocations = function () {
         locationsVm.searchActivityLocations(vm.newActivity());
 
         vm.newActivity('');
-    };
+    }
 
 };
