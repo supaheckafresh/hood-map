d5f0c5b0352760a029cca00682622ab5767cb8f0
supaheckafresh <neilstrain@gmail.com>
2016-02-25 11:23:00 -0800

    Bug Fix: master.viewmodel.js was missing. Added back manually

---
+++ dev/js/master.viewmodel.js
@@ -0,0 +1,16 @@
+(function () {
+
+    'use strict';
+
+    var MasterViewModel = function () {
+        var vm = this;
+
+        vm.MapViewModel = new MapViewModel();
+        vm.LocationsViewModel = new LocationsViewModel(vm.MapViewModel);
+        vm.ActivitiesViewModel = new ActivitiesViewModel(vm.MapViewModel, vm.LocationsViewModel);
+    };
+
+    ko.applyBindings(new MasterViewModel());
+
+}());
+
