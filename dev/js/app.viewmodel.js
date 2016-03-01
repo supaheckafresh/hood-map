1977779c133a8f8d23e206cebfdc63c5ebee5e05
supaheckafresh <neilstrain@gmail.com>
2016-03-01 12:15:51 -0800

    Display sidebar component

--- dev/js/app.viewmodel.js
+++ dev/js/app.viewmodel.js
@@ -20,16 +20,22 @@
     var app = new AppViewModel();
 
     var $searchbar = $('<div>');
-    $searchbar.load('./build/components/searchbar/searchbar.html #search-view', function(){
+    $searchbar.load('./build/components/searchbar/searchbar.html #searchbar', function() {
         var searchbar = $(this).prop('outerHTML');
 
         $('#overlay-search').append(searchbar);
 
-        ko.applyBindings(app, document.getElementById('search-view'));
+        ko.applyBindings(app, document.getElementById('searchbar'));
     });
 
-    //var $sidebar = $('<div>');
-    //$sidebar.load('./build/components/sidebar/sidebar.html')
+    var $sidebar = $('<div>');
+    $sidebar.load('./build/components/sidebar/sidebar.html #sidebar', function () {
+        var sidebar = $(this).prop('outerHTML');
+
+        $('#overlay-sidebar').append(sidebar);
+
+        ko.applyBindings(app, document.getElementById('sidebar'));
+    });
 
 
     // Use jQuery-UI to make the search and sidebar UIs draggable.
