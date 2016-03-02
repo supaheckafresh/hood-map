f63c1db061035e65fefb610d567ff57d9527a404
supaheckafresh <neilstrain@gmail.com>
2016-03-02 11:34:51 -0800

    Add Geolocation() constructor properties

--- dev/js/map/geolocation.constructor.js
+++ dev/js/map/geolocation.constructor.js
@@ -2,8 +2,13 @@
 
     'use strict';
 
-    var Geolocation = function Geolocation() {
-
+    var Geolocation = function Geolocation(data) {
+        this.locationName = ko.observable(data.locationName());
+        this.center = ko.observable(data.center);
+        this.queryTime = ko.observable(_.now());
+        this.activities = ko.observable(data.activities());
+        this.active = ko.observable(false);
+        this.zoom = ko.observable(data.zoom);
     }
 
 }());
